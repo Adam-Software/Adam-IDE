81553527fa1533ac1800b539ead11c8468a4da83
Alexey Nesterov <vertigra.developer@gmail.com>
2024-03-16 13:06:17 +1000

    Remove obsolete code

--- AdamController.Core/Dialog.ViewModels/SettingsViewModel.cs
+++ AdamController.Core/Dialog.ViewModels/SettingsViewModel.cs
@@ -1,28 +1,108 @@
 ﻿using AdamController.Core.Mvvm;
+using FolderBrowserEx;
 using Prism.Commands;
-using Prism.Mvvm;
-using Prism.Services.Dialogs;
-using System;
+
 
 namespace AdamController.Core.Dialog.ViewModels
 {
     public class SettingsViewModel : DialogViewModelBase
-
     {
+        private readonly IFolderBrowserDialog IFolderBrowser;
 
         #region ~
 
         public SettingsViewModel()
         {
             Title  = "Настройки Adam IDE";
+            IFolderBrowser = new FolderBrowserDialog();
         }
 
         #endregion
 
-        #region Navigation
+        #region Command
 
+        private DelegateCommand<string> showOpenFolderDialogCommand;
+        public DelegateCommand<string> ShowOpenFolderDialogCommand => showOpenFolderDialogCommand ??= new DelegateCommand<string>(obj =>
+        {
+            string dialogPathType = obj;
 
+            if (string.IsNullOrEmpty(dialogPathType)) return;
 
+            switch (dialogPathType)
+            {
+                case "workspace":
+                    {
+                        string savedPath = OpenFolderDialog("Выберите директорию для сохранения рабочих областей", Properties.Settings.Default.SavedUserWorkspaceFolderPath);
+
+                        if (string.IsNullOrEmpty(savedPath)) return;
+
+                        Properties.Settings.Default.SavedUserWorkspaceFolderPath = savedPath;
+                        break;
+                    }
+                case "toolbox":
+                    {
+                        string savedPath = OpenFolderDialog("Выберите директорию для сохранения панелей инструментов", Properties.Settings.Default.SavedUserToolboxFolderPath);
+
+                        if (string.IsNullOrEmpty(savedPath)) return;
+
+                        Properties.Settings.Default.SavedUserToolboxFolderPath = savedPath;
+                        break;
+                    }
+                case "blocks":
+                    {
+                        string savedPath = OpenFolderDialog("Выберите директорию для сохранения блоков", Properties.Settings.Default.SavedUserCustomBlocksFolderPath);
+
+                        if (string.IsNullOrEmpty(savedPath)) return;
+
+                        Properties.Settings.Default.SavedUserCustomBlocksFolderPath = savedPath;
+                        break;
+                    }
+                case "script":
+                    {
+                        string savedPath = OpenFolderDialog("Выберите директорию для сохранения скриптов", Properties.Settings.Default.SavedUserScriptsFolderPath);
+
+                        if (string.IsNullOrEmpty(savedPath)) return;
+
+                        Properties.Settings.Default.SavedUserScriptsFolderPath = savedPath;
+                        break;
+                    }
+                case "testResult":
+                    {
+                        string savedPath = OpenFolderDialog("Выберите директорию для сохранения результатов тестов", Properties.Settings.Default.SavedResultsNetworkTestsFolderPath);
+
+                        if (string.IsNullOrEmpty(savedPath)) return;
+
+                        Properties.Settings.Default.SavedResultsNetworkTestsFolderPath = savedPath;
+                        break;
+                    }
+
+                default:
+                    break;
+            }
+        });
+
+        #endregion
+
+        #region Methods
+
+        private string OpenFolderDialog(string title, string openDialogPath)
+        {
+            IFolderBrowser.Title = title;
+            IFolderBrowser.InitialFolder = openDialogPath;
+            IFolderBrowser.AllowMultiSelect = false;
+
+            //if (IFolderBrowser.ShowDialog() == DialogResult.Cancel)
+            //{
+                //SettingWindowActivated = true;
+            //    return null;
+            //}
+
+            //SettingWindowActivated = true;
+
+            return IFolderBrowser.SelectedFolder;
+        }
+
+        
         #endregion
     }
 }
