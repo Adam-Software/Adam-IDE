b78d77861a0cc74b02fbe61c241c964438042ef0
Alexey Nesterov <vertigra.developer@gmail.com>
2024-05-07 12:46:26 +1000

    Close #50 p 1.1

--- Modules/AdamController.Modules.FlayoutsRegion/ViewModels/WebApiSettingsViewModel.cs
+++ Modules/AdamController.Modules.FlayoutsRegion/ViewModels/WebApiSettingsViewModel.cs
@@ -1,13 +1,29 @@
 ﻿using AdamController.Controls.CustomControls.Mvvm.FlyoutContainer;
+using AdamController.Services.Interfaces;
 
 namespace AdamController.Modules.FlayoutsRegion.ViewModels
 {
     public class WebApiSettingsViewModel : FlyoutBase
     {
-        public WebApiSettingsViewModel() 
+        private readonly ICultureProvider mCultureProvider;
+
+        public WebApiSettingsViewModel(ICultureProvider cultureProvider) 
         {
-            BorderThickness = 0;
-            Header = "Настройки WebApi";
+            mCultureProvider = cultureProvider; 
+            BorderThickness = 1;   
         }
+
+        #region Navigation
+
+        protected override void OnChanging(bool isOpening)
+        {
+            if(isOpening)
+            {
+                Header = mCultureProvider.FindResource("WebApiSettingsView.ViewModel.Flyout.Header");
+                return;
+            }
+        }
+
+        #endregion
     }
 }
