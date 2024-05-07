cc38a14e27b9573286c46094332f9b36294d9f95
Alexey Nesterov <vertigra.developer@gmail.com>
2024-05-07 11:35:11 +1000

    Close #50 p 1.3

--- Modules/AdamController.Modules.FlayoutsRegion/ViewModels/PortSettingsViewModel.cs
+++ Modules/AdamController.Modules.FlayoutsRegion/ViewModels/PortSettingsViewModel.cs
@@ -1,13 +1,30 @@
 ﻿using AdamController.Controls.CustomControls.Mvvm.FlyoutContainer;
+using AdamController.Services.Interfaces;
 
 namespace AdamController.Modules.FlayoutsRegion.ViewModels
 {
     public class PortSettingsViewModel : FlyoutBase
     {
-        public PortSettingsViewModel() 
+
+        #region Services
+
+        private readonly ICultureProvider mCultureProvider;
+
+        #endregion
+
+        public PortSettingsViewModel(ICultureProvider cultureProvider) 
         {
-            BorderThickness = 0;
-            Header = "Настройки портов";
+            BorderThickness = 1;
+            mCultureProvider = cultureProvider;
+        }
+
+        protected override void OnChanging(bool isOpening)
+        {
+            if (isOpening)
+            {
+                Header = mCultureProvider.FindResource("PortSettingsView.ViewModel.Flyout.Header");
+                return;
+            }
         }
     }
 }
