db5301a9b2d6cf660bac04d8aa2270f3a56a1c34
Alexey Nesterov <vertigra.developer@gmail.com>
2024-06-11 11:41:54 +1000

    Close #87 p.1

--- AdamController/App.xaml.cs
+++ AdamController/App.xaml.cs
@@ -52,9 +52,10 @@ using System.IO;
 
 namespace AdamController
 {
-    public partial class App : PrismApplication
+    public partial class App : Application
     {
-        #region ~
+
+        private readonly Bootstrapper mBootstrapper = new();
 
         public App()
         {
@@ -62,191 +63,11 @@ namespace AdamController
             LoadSharedFFmpegLibrary();
         }
 
-        #endregion
-
-        protected override Window CreateShell()
+        protected override void OnStartup(StartupEventArgs e)
         {
-            MainWindow window = Container.Resolve<MainWindow>();
-            return window;
-        }
-
-        protected override void RegisterTypes(IContainerRegistry containerRegistry)
-        {
-            containerRegistry.RegisterSingleton<IFlyoutStateChecker, FlyoutStateChecker>();
-
-            containerRegistry.RegisterSingleton<ICultureProvider, CultureProvider>();
-            containerRegistry.RegisterSingleton<IFileManagmentService, FileManagmentService>();
-            containerRegistry.RegisterSingleton<IFolderManagmentService, FolderManagmentService>();
-
-            containerRegistry.RegisterSingleton<IAvalonEditService>(containerRegistry =>
-            {
-                IFileManagmentService fileManagment = containerRegistry.Resolve<IFileManagmentService>();
-                AvalonEditService avalonService = new(fileManagment);
-                return avalonService;
-            });
-
-            containerRegistry.RegisterSingleton<IWebViewProvider, WebViewProvider>();
-            containerRegistry.RegisterSingleton<ISubRegionChangeAwareService, SubRegionChangeAwareService>();
-            containerRegistry.RegisterSingleton<IStatusBarNotificationDeliveryService, StatusBarNotificationDeliveryService>();
-
-            containerRegistry.RegisterSingleton<IFlyoutManager>(containerRegistry =>
-            {
-                DryIoc.IContainer container = containerRegistry.GetContainer();
-                IRegionManager regionManager = containerRegistry.Resolve<IRegionManager>();
-
-                return new FlyoutManager(container, regionManager);
-            });
-
-            containerRegistry.RegisterSingleton<ITcpClientService>(() =>
-            {
-                TcpClientOption option = new()
-                {
-                    ReconnectCount = Settings.Default.ReconnectQtyComunicateTcpClient,
-                    ReconnectTimeout = Settings.Default.ReconnectTimeoutComunicateTcpClient
-                };
-
-                string ip = Settings.Default.ServerIP;
-                int port = Settings.Default.TcpConnectStatePort;
-                
-                TcpClientService client;
-
-                if (!string.IsNullOrEmpty(ip))
-                {
-                    client = new(ip, port, option);
-                }
-                else
-                {
-                    client = new("127.0.0.1", port, option);
-                }
-
-                
-                return client;
-            });
-
-            containerRegistry.RegisterSingleton<IUdpClientService>(() =>
-            {
-                IPAddress ip = IPAddress.Any;
-                int port = int.Parse(Settings.Default.MessageDataExchangePort);
-
-                UdpClientService client = new(ip, port)
-                {
-                    OptionDualMode = true,
-                    OptionReuseAddress = true
-                };
-
-                return client;
-            });
-
-            containerRegistry.RegisterSingleton<IUdpServerService>(() =>
-            {
-                IPAddress ip = IPAddress.Any;
-                int port = Settings.Default.LogServerPort;
-
-                UdpServerService server = new(ip, port)
-                {
-                    OptionDualMode = true,
-                    OptionReuseAddress = true
-                };
-
-                return server;
-            });
-
-            containerRegistry.RegisterSingleton<IWebSocketClientService>(() =>
-            {
-                string ip = Settings.Default.ServerIP;
-                int port = Settings.Default.SoketServerPort;
-                Uri uri;
-
-                if (!string.IsNullOrEmpty(ip))
-                {
-                    uri = new($"ws://{ip}:{port}/adam-2.7/movement");
-                }
-                else
-                {
-                    uri = new($"ws://127.0.0.1:9001/adam-2.7/movement");
-                }
-
-                // debug, use only with debug server, which runs separately, not as a service
-                //Uri uri = new($"ws://{Settings.Default.ServerIP}:9001/adam-2.7/movement");
-
-                // work in production, connect to socket-server run as service
-                // Uri uri = new($"ws://{ip}:{port}/adam-2.7/movement");
-
-                WebSocketClientService client = new(uri);
-                return client;
-            });
-
-            containerRegistry.RegisterSingleton<IWebApiService>(() =>
-            {
-                string ip = Settings.Default.ServerIP;
-                int port = Settings.Default.ApiPort;
-                string login = Settings.Default.ApiLogin;
-                string password = Settings.Default.ApiPassword;
-
-                if (string.IsNullOrEmpty(ip))
-                {
-                    ip = "127.0.0.1";
-                }
-                
-                WebApiService client = new(ip, port, login, password);
-                return client;
-
-            });
-
-            containerRegistry.RegisterSingleton<ICommunicationProviderService>(containerRegistry =>
-            {
-                ITcpClientService tcpClientService = containerRegistry.Resolve<ITcpClientService>();
-                IUdpClientService udpClientService = containerRegistry.Resolve<IUdpClientService>();
-                IUdpServerService udpServerService = containerRegistry.Resolve<IUdpServerService>();
-                IWebSocketClientService socketClientService = containerRegistry.Resolve<IWebSocketClientService>();
-                
-                CommunicationProviderService communicationProvider = new(tcpClientService, udpClientService, udpServerService, socketClientService);
-
-                return communicationProvider;
-            });
-
-            containerRegistry.RegisterSingleton<IPythonRemoteRunnerService>(containerRegistry =>
-            {
-                IUdpClientService udpClient = containerRegistry.Resolve<IUdpClientService>();
-
-                PythonRemoteRunnerService remoteRunnerService = new(udpClient);
-                return remoteRunnerService;
-            });
-
-            containerRegistry.RegisterSingleton<IThemeManagerService, ThemeManagerService>();
-            containerRegistry.RegisterSingleton<IControlHelper>(containerRegistry =>
-            {
-                bool isVideoShowLastValue = Settings.Default.ShowVideo;
-                return new ControlHelper(isVideoShowLastValue);
-            });
-
-            containerRegistry.RegisterSingleton<IVideoViewProvider, VideoViewProvider>();
-
-            RegisterDialogs(containerRegistry);
-        }
-
-        private static void RegisterDialogs(IContainerRegistry containerRegistry)
-        {
-            // used for call system dialog for open save/open/select file/folder (Microsoft.Win32 dialogs)
-            containerRegistry.RegisterSingleton<ISystemDialogService, SystemDialogService>();
-
-            //Dialog boxes are not used, but implemented
-            //containerRegistry.RegisterDialog<SettingsView, SettingsViewModel>();
-        }
-
-        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
-        {
-            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
-
-            regionAdapterMappings.RegisterMapping(typeof(FlyoutsControl), Container.Resolve<FlyoutsControlRegionAdapter>());
-        }
-
-        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
-        {
-            moduleCatalog.AddModule<MenuRegionModule>();
-            moduleCatalog.AddModule<ContentRegionModule>();
-            moduleCatalog.AddModule<StatusBarRegionModule>();
-            moduleCatalog.AddModule<FlayoutsRegionModule>();
+            base.OnStartup(e);
+            
+            mBootstrapper.Run();
         }
 
         protected override void OnExit(ExitEventArgs e)
@@ -259,32 +80,8 @@ namespace AdamController
         private void OnAppCrashOrExit()
         {
             Unsubscribe();
-            DisposeServices();
+            mBootstrapper.Dispose();
             Current.Shutdown();
-        }
-
-        /// <summary>
-        /// The priority of resource release is important!
-        /// FirstRun/LastStop
-        /// </summary>
-        private void DisposeServices()
-        {
-            Container.Resolve<IThemeManagerService>().Dispose();
-            Container.Resolve<IFileManagmentService>().Dispose();
-            Container.Resolve<IFolderManagmentService>().Dispose();
-            Container.Resolve<IAvalonEditService>().Dispose();
-
-            Container.Resolve<ISubRegionChangeAwareService>().Dispose();
-            Container.Resolve<IStatusBarNotificationDeliveryService>().Dispose();
-            Container.Resolve<IWebViewProvider>().Dispose();
-            Container.Resolve<ISystemDialogService>().Dispose();
-
-            Container.Resolve<IPythonRemoteRunnerService>().Dispose();
-            Container.Resolve<ICommunicationProviderService>().Dispose();
-            Container.Resolve<IWebApiService>().Dispose();
-            Container.Resolve<ICultureProvider>().Dispose();
-
-            Container.Resolve<IControlHelper>().Dispose();
         }
 
         #region Subscribes
@@ -302,11 +99,13 @@ namespace AdamController
 
         #endregion
 
-        #region On raise event
+        #region OnRaise event
+        
         private void OnPropertyChange(object sender, PropertyChangedEventArgs e)
         {
             Settings.Default.Save();
         }
+
         #endregion
 
         #region Intercepting Unhandled Exception
@@ -322,7 +121,7 @@ namespace AdamController
                 ShowUnhandledException(args.Exception, "TaskScheduler.UnobservedTaskException", false);
 
             // Catch exceptions from a single specific UI dispatcher thread.
-            Dispatcher.UnhandledException += (sender, args) =>
+            Current.Dispatcher.UnhandledException += (sender, args) =>
             {
                 // If we are debugging, let Visual Studio handle the exception and take us to the code that threw it.
                 if (!Debugger.IsAttached)
@@ -358,8 +157,6 @@ namespace AdamController
                 OnAppCrashOrExit();
             }
         }
-
-
 
         #endregion
 
